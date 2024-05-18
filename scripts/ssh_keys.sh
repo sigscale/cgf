9b63a0b7d4a62442dfab8570fb0458ccbae4caf4
Vance Shipley <vances@sigscale.org>
2024-05-18 14:08:16 +0800

    shell script for ssh key management

---
+++ scripts/ssh_keys.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Create SSH keys
+
+ALGORITHMS="rsa dsa ecdsa ed25519"
+
+system_keys() {
+	if ! [ -d ${system_dir} ] ;
+	then
+		mkdir -p ${system_dir}
+	fi
+	for algo in ${ALGORITHMS};
+	do
+		if ! [ -f ${system_dir}/ssh_host_${algo}_key ] ;
+		then
+			ssh-keygen -t ${algo} -N '' -f ${system_dir}/ssh_host_${algo}_key
+		fi
+	done
+}
+
+user_keys() {
+	for username in $@;
+	do
+		if ! [ -d ${user_dir}/${username} ] ;
+		then
+			mkdir -p ${user_dir}/${username}
+		fi
+		for algo in ${ALGORITHMS};
+		do
+			if ! [ -f ${user_dir}/${username}/id_${algo} ] ;
+			then
+				ssh-keygen -t ${algo} -N '' -f ${user_dir}/${username}/id_${algo}
+			fi
+			cat ${user_dir}/${username}/id_${algo}.pub >> ${user_dir}/${username}/authorized_hosts
+		done
+	done
+}
+
+usage() {
+	echo "usage: $0 [--system-dir ~/ssh/system]"
+	echo "usage: $0 [--user-dir ~/ssh/system/user] USERNAME ..."
+	echo "usage: $0 --help"
+}
+
+system_dir="${HOME}/ssh/system"
+user_dir="${HOME}/ssh/user"
+
+while [ -n "$1" ];
+do
+	case $1 in
+		--system-dir )
+			shift
+			system_dir="$1"
+			;;
+		--user-dir )
+			shift
+			user_dir="$1"
+			;;
+		--help )
+			usage
+			exit
+			;;
+		* )
+			break
+			;;
+	esac
+	shift
+done
+
+if [ $# -gt 0 ] ;
+then
+	user_keys $@
+else
+	system_keys
+fi
+
