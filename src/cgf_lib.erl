d7929635f32dc74516f378198fba1c9afa6aa361
Vance Shipley <vances@sigscale.org>
2024-09-12 00:53:16 +0800

    tbcd, address abd date-time codec functions in library module

--- src/cgf_lib.erl
+++ src/cgf_lib.erl
@@ -22,7 +22,8 @@
 -copyright('Copyright (c) 2024 SigScale Global Inc.').
 -author('Vance Shipley <vances@sigscale.org>').
 
--export([octet_string/1, bcd_dn/1, octet_ip_address/1]).
+-export([octet_string/1, tbcd/1, bcd_dn/1,
+		octet_ip_address/1, ip_address/1, bcd_date_time/1]).
 
 %%----------------------------------------------------------------------
 %%  The cgf_lib public API
@@ -42,6 +43,42 @@ octet_string(<<UpperNibble:4, LowerNibble:4, Rest/binary>>, Acc) ->
 	Acc1 = <<Acc/binary, Hex1, Hex2>>,
 	octet_string(Rest, Acc1);
 octet_string(<<>>, Acc) ->
+	Acc.
+
+-spec tbcd(Digits) -> Digits
+	when
+		Digits :: [Digit] | binary(),
+		Digit :: $0..$9 | $* | $# | $a | $b | $c.
+%% @doc CODEC for TBCD-String.
+%%
+%% 	The Telephony Binary Coded Decimal String (TBCD)
+%% 	type is used to store dialing digits in a compact
+%% 	form. Digits are "0123456789*#abc"..
+tbcd(Digits) when is_binary(Digits) ->
+	tbcd(Digits, []);
+tbcd(Digits) when is_list(Digits) ->
+	tbcd(Digits, <<>>).
+%% @hidden
+tbcd(<<15:4, A:4>>, Acc) ->
+	lists:reverse([digit(A) | Acc]);
+tbcd(<<A2:4, A1:4, Rest/binary>>, Acc)
+		when A1 >= 0, A1 < 15, A2 >= 0, A2 < 15 ->
+	tbcd(Rest, [digit(A2), digit(A1) | Acc]);
+tbcd(<<>>, Acc) ->
+	lists:reverse(Acc);
+tbcd([A], Acc) when ((A >= $0) and (A =< $9)) or (A == $*)
+		or (A == $#) or ((A >= $a) and (A =<$c)) ->
+	B = digit(A),
+	<<Acc/binary, 15:4, B:4>>;
+tbcd([A2, A1 | T], Acc)
+		when (((A1 >= $0) or (A1 =< $9)) or (A1 == $*)
+		or (A1 == $#) or ((A1 >= $a) and (A1 =<$c))),
+		(((A2 >= $0) and (A2 =< $9)) or (A2 == $*)
+		or (A2 == $#) or ((A2 >= $a) and (A2 =<$c))) ->
+	B1 = digit(A1),
+	B2 = digit(A2),
+	tbcd(T, <<Acc/binary, B1:4, B2:4>>);
+tbcd([], Acc) ->
 	Acc.
 
 -spec bcd_dn(BCD) -> PartyAddress
@@ -75,15 +112,73 @@ bcd_dn(<<N2:4, N1:4, Rest/binary>>,
 bcd_dn(<<>>, Acc) ->
 	Acc.
 
--spec octet_ip_address(Octets) -> String
+-spec octet_ip_address(Octets) -> Address
 	when
 		Octets :: binary(),
-		String :: list().
+		Address :: string().
 %% @doc Converts a `OCTET STRING' to an `IPV4' or an `IPv6' address in string format.
 octet_ip_address(<<A:8, B:8, C:8, D:8>>) ->
 	inet:ntoa({A, B, C, D});
 octet_ip_address(<<H1:16, H2:16, H3:16, H4:16, H5:16, H6:16, H7:16, H8:16>>) ->
 	inet:ntoa({H1, H2, H3, H4, H5, H6, H7, H8}).
+
+-spec ip_address(IPAddress) -> Address
+	when
+		IPAddress :: {iPBinaryAddress, IPBinaryAddress}
+				| {iPTextRepresentedAddress, IPTextRepresentedAddress},
+		IPBinaryAddress :: {iPBinV4Address, IPBinV4Address}
+				| {iPBinV6Address, IPBinV6AddressWithOrWithoutPrefixLength},
+		IPBinV4Address :: binary(),
+		IPBinV6Address :: binary(),
+		IPBinV6AddressWithOrWithoutPrefixLength :: {iPBinV6Address, IPBinV6Address}
+				| {iPBinV6AddressWithPrefix, IPBinV6AddressWithPrefixLength},
+		IPBinV6AddressWithPrefixLength :: #{iPBinV6Address := IPBinV6Address,
+				pDPAddressPrefixLength => PDPAddressPrefixLength},
+		PDPAddressPrefixLength :: 0..128,
+		IPTextRepresentedAddress :: {iPTextV4Address, string()}
+				| {iPTextV6Address, string()},
+		Address :: string().
+%% @doc Convert an ASN.1 `IPAddress' to an IPv4/IPv6 address in string format.
+ip_address({iPBinaryAddress, IPBinaryAddress}) ->
+	ip_address1(IPBinaryAddress);
+ip_address({iPTextRepresentedAddress, IPTextRepresentedAddress}) ->
+	ip_address4(IPTextRepresentedAddress).
+%% @hidden
+ip_address1({iPBinV4Address, IPBinV4Address}) ->
+	cgf_lib:octet_ip_address(IPBinV4Address);
+ip_address1({iPBinV6Address, IPBinV6AddressWithOrWithoutPrefixLength}) ->
+	ip_address2(IPBinV6AddressWithOrWithoutPrefixLength).
+%% @hidden
+ip_address2({iPBinV6Address, IPBinV6Address}) ->
+	cgf_lib:octet_ip_address(IPBinV6Address);
+ip_address2({iPBinV6AddressWithPrefix, IPBinV6AddressWithPrefixLength}) ->
+	ip_address3(IPBinV6AddressWithPrefixLength).
+%% @hidden
+ip_address3(#{iPBinV6Address := IPBinV6Address,
+		pDPAddressPrefixLength := PDPAddressPrefixLength}) ->
+	cgf_lib:octet_ip_address(IPBinV6Address)
+			++ "/" ++ integer_to_list(PDPAddressPrefixLength);
+ip_address3(#{iPBinV6Address := IPBinV6Address}) ->
+	cgf_lib:octet_ip_address(IPBinV6Address).
+%% @hidden
+ip_address4({iPTextV4Address, Address}) ->
+	Address;
+ip_address4({iPTextV6Address, Address}) ->
+	Address.
+
+-spec bcd_date_time(Binary) -> ISO6801
+	when
+		Binary :: binary(),
+		ISO6801 :: string().
+%% @doc Convert `OCTET STRING' to `date-time'.
+bcd_date_time(<<Year2:4, Year1:4, Month2:4, Month1:4, Day2:4, Day1:4,
+		Hour2:4, Hour1:4, Minute2:4, Minute1:4, Second2:4, Second1:4,
+		Sign:8, TzHour2:4, TzHour1:4, TzMinute2:4, TzMinute1:4>>)
+		when Sign == $-; Sign == $+ ->
+	io_lib:fwrite("20~b~b-~b~b-~b~bT~b~b:~b~b:~b~b~c~b~b:~b~b",
+			[Year2, Year1, Month2, Month1, Day2, Day1,
+      Hour2, Hour1, Minute2, Minute1, Second2, Second1,
+      Sign, TzHour2, TzHour1, TzMinute2, TzMinute1]).
 
 %%----------------------------------------------------------------------
 %%  Internal functions
